e30b3f77c02f89f8a4c36cfebca621dc7ef4785c
AasawariM <aasawari.m.work@gmail.com>
2025-11-16 11:25:29 +0530

    data types in java

--- FirstProgram/Hello.java
+++ FirstProgram/Hello.java
@@ -1,6 +1,19 @@
-public class Hello {
+import java.awt.*;
+
+class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        int a = 21;
+        int b = a;
+        a = 32;
+        System.out.println(a);
+        System.out.println(b);
+
+        Point point1 = new Point(3, 2);
+        System.out.println(point1);
+        Point point2 = point1;
+        point1.x = 21;
+        System.out.println(point2);
     }
 }
