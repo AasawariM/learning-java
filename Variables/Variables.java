13c1d463a62aa64cccea8ac95eba42666f30a2a1
AasawariM <aasawari.m.work@gmail.com>
2025-11-17 23:28:37 +0530

    variable types in java

--- Variables/Variables.java
+++ Variables/Variables.java
@@ -1,5 +1,35 @@
+class Employee {
+    int id;       // instance variable
+    String name;  // instance variable
+
+    static String company = "XYZ TechCorp"; // static variable
+
+    public void empDetails(int EmpId, String EmpName) {
+        id = EmpId;
+        name = EmpName;
+
+        int bonus = 5000; // local variable
+
+        System.out.println("ID: " + id + ", Name: " + name 
+            + ", Company: " + company 
+            + ", Bonus Received: " + bonus);
+    }
+}
+
 public class Variables {
- public static void main(String[] args) {
-    
- }   
+    public static void main(String[] args) {
+
+        Employee e = new Employee();
+
+        // Accessing static variable using class name
+        System.out.println("Company Name: " + Employee.company);
+
+        // Updating static variable
+        Employee.company = "AgriTech";
+
+        System.out.println("Updated Company Name: " + Employee.company);
+
+        // Instance method call
+        e.empDetails(1, "Bob");
+    }
 }
