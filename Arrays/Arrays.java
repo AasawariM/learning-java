36341a53495c261cdb0bb12150b2b7aa4f4ea7fa
AasawariM <aasawari.m.work@gmail.com>
2025-11-17 16:17:44 +0530

    jagged array example added

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -81,19 +81,54 @@ public class Arrays {
             System.out.println();
         }
 
-
-
-        // instead of using normal for loop for printing array we can use enhanced for loop
+        // instead of using normal for loop for printing array we can use enhanced for
+        // loop
 
         System.out.println("-------------Enhanced for Loop---------------");
 
         for (int row[] : nums) // here n is not a simple variable it is an one dimensional array
         {
-            for(int value: row){
-                System.out.print(value+" ");
+            for (int value : row) {
+                System.out.print(value + " ");
             }
             System.out.println();
+        }
 
+        // Jagged Array
+        System.out.println("---------------Jagged Array------------------");
+        int jagged[][] = new int[3][]; // jagged
+        // You must assign the length of each row individually.
+        jagged[0] = new int[3];
+        jagged[1] = new int[4];
+        jagged[2] = new int[2];
+
+        jagged[0][0] = 1;
+        jagged[0][1] = 2;
+        jagged[0][2] = 3;
+
+        jagged[1][0] = 1;
+        jagged[1][1] = 2;
+        jagged[1][2] = 3;
+        jagged[1][3] = 3;
+        
+        jagged[2][0] = 1;
+        jagged[2][1] = 2;
+
+        
+        // with normal for loop
+        for (int i = 0; i < jagged.length; i++) {
+            for (int j = 0; j < jagged[i].length; j++) {
+                System.out.print(jagged[i][j] + " ");
+            }
+            System.out.println();
+        }
+        // with enhanced for loop
+        System.out.println("------Enhanced For Loop----------");
+        for (int[] item : jagged) {
+            for (int value : item) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
         }
 
     }
